340628dac0ac3b08b935b4a10a6d04c906140d91
chenweida <494679975@qq.com>
2017-05-16 17:49:44 +0800

    参数配置

--- web-gateway/src/main/java/com/yihu/jw/WebGateWayAppliaction.java
+++ web-gateway/src/main/java/com/yihu/jw/WebGateWayAppliaction.java
@@ -8,7 +8,6 @@ import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
-import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 /**
