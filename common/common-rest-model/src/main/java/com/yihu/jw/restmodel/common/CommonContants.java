bf188be236d588b35065e93dbef469ba1b2e27d5
chenweida <494679975@qq.com>
2017-05-22 14:01:01 +0800

    修改全局变量的位置

--- common/common-rest-model/src/main/java/com/yihu/jw/restmodel/common/CommonContants.java
+++ common/common-rest-model/src/main/java/com/yihu/jw/restmodel/common/CommonContants.java
@@ -5,6 +5,11 @@ package com.yihu.jw.restmodel.common;
  * 公共的常量类
  */
 public class CommonContants {
+
+    //微服务的id start
+    public static final String svr_base = "svr-base";
+    public static final String svr_gateway = "svr-gateway";
+
     public static final Integer common_error_code= -10000; //系统异常
 
     public static final Integer common_error_params_code= -10100; //接口入参异常
--- web-gateway/src/main/java/com/yihu/jw/fegin/PatientFegin.java
+++ web-gateway/src/main/java/com/yihu/jw/fegin/PatientFegin.java
@@ -3,6 +3,7 @@ package com.yihu.jw.fegin;
 import com.yihu.jw.commnon.Contants;
 import com.yihu.jw.fegin.fallback.PatientFeginFallback;
 import com.yihu.jw.fegin.fallbackfactory.PatientFeginFallbackFactory;
+import com.yihu.jw.restmodel.common.CommonContants;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.cloud.netflix.feign.FeignClient;
@@ -14,7 +15,7 @@ import org.springframework.web.bind.annotation.RequestParam;
  * Created by chenweida on 2017/5/10.
  */
 @FeignClient(
-        name = Contants.svr_base // name值是eurika的实例名字
+        name = CommonContants.svr_base // name值是eurika的实例名字
        // ,fallback = PatientFeginFallback.class// fallback是请求超时或者错误的回调函数
         ,fallbackFactory  =PatientFeginFallbackFactory.class
 //        ,configuration =   //可以配置当个fegin的配置 例如禁用单个feign的hystrix
