cf01d649af6b5d6396a7faf868962edb80b8fa28
chenweida <494679975@qq.com>
2017-05-22 15:31:11 +0800

    新增给saas配置模块 和给模块配置功能

--- svr/svr-base/src/main/java/com/yihu/jw/base/service/ModuleService.java
+++ svr/svr-base/src/main/java/com/yihu/jw/base/service/ModuleService.java
@@ -2,16 +2,23 @@ package com.yihu.jw.base.service;
 
 import com.yihu.jw.base.dao.ModuleDao;
 import com.yihu.jw.base.dao.ModuleDao;
+import com.yihu.jw.base.dao.SaasModuleDao;
 import com.yihu.jw.base.model.Module;
 import com.yihu.jw.base.model.Module;
+import com.yihu.jw.base.model.SaasModule;
 import com.yihu.jw.mysql.query.BaseJpaService;
 import com.yihu.jw.restmodel.base.BaseContants;
+import com.yihu.jw.restmodel.base.MModule;
 import com.yihu.jw.restmodel.common.CommonContants;
 import com.yihu.jw.restmodel.exception.ApiException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.StringUtils;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by chenweida on 2017/5/19.
@@ -20,6 +27,11 @@ import org.springframework.util.StringUtils;
 public class ModuleService extends BaseJpaService<Module, ModuleDao> {
     @Autowired
     private ModuleDao moduleDao;
+    @Autowired
+    private SaasModuleDao saasModuleDao;
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
 
     @Transactional
     public Module createModule(Module module) throws ApiException {
@@ -72,5 +84,26 @@ public class ModuleService extends BaseJpaService<Module, ModuleDao> {
             throw new ApiException(BaseContants.Module.message_fail_code_no_exist, CommonContants.common_error_params_code);
         }
         module.setStatus(-1);
+    }
+
+    @Transactional
+    public void assignModule(String saasCode, String moduleCodes) {
+        //先删除原来已经分配好的模块
+        saasModuleDao.deleteBySaasCode(saasCode);
+        //分配新的模块
+        String [] moduleCodeArr=moduleCodes.split(",");
+        List<SaasModule> saasModuleList=new ArrayList<>();
+        for(String moduleCode:moduleCodeArr){
+            SaasModule saasModule=new SaasModule();
+            saasModule.setModuleId(moduleCode);
+            saasModule.setSaasId(saasCode);
+            saasModuleList.add(saasModule);
+        }
+        saasModuleDao.save(saasModuleList);
+    }
+
+    public List<MModule> getSaasModules(String saasCode) {
+        String sql=" select m.code,m.parent_code,m.name from base_module m,base_saas_module sm where m.code=sm.module_id and m.status=0 and sm.saas_id=?";
+       return jdbcTemplate.queryForList(sql,MModule.class,saasCode);
     }
 }
