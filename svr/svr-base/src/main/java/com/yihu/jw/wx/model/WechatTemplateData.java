86ddf6f02eda43894fb061553c33105de5740c1f
chenyongxing <123456>
2017-05-25 14:05:20 +0800

    微信公众号接口开发

--- svr/svr-base/src/main/java/com/yihu/jw/wx/model/WechatTemplateData.java
+++ svr/svr-base/src/main/java/com/yihu/jw/wx/model/WechatTemplateData.java
@@ -1,13 +1,13 @@
 package com.yihu.jw.wx.model;
 
 
-import com.yihu.jw.base.model.base.IdEntity;
+import java.io.Serializable;
 
 /**
  * 微信消息模板
  * @author George
  */
-public class WechatTemplateData extends IdEntity {
+public class WechatTemplateData implements Serializable {
 	private static final long serialVersionUID = -7399054549159698617L;
 	private String value; 
     private String color;
