48068345ec18534790090170c2315b9a8c4a732b
chenweida <494679975@qq.com>
2017-05-31 14:34:47 +0800

    新增后台管理系统网关

---
+++ manage-gateway/src/main/java/com/yihu/jw/manage/SvrManageAppliction.java
@@ -0,0 +1,27 @@
+package com.yihu.jw.manage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+/**
+ * Created by chenweida on 2017/5/31.
+ */
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
+//@SpringBootApplication
+@EnableDiscoveryClient//服务注册到发现服务
+@EnableHystrix //启动断路器
+@EnableZuulProxy //启动zuul代理 路由
+@EnableFeignClients //声名式的客户端
+@EnableCircuitBreaker
+public class SvrManageAppliction {
+    public static void main(String[] args) {
+        SpringApplication.run(SvrManageAppliction.class, args);
+    }
+}
