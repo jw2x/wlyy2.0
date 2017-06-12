5af41ba938535ec0adc3b0e0820be1d197486686
chenweida <494679975@qq.com>
2017-06-12 09:04:43 +0800

    代码修改

--- svr/svr-manage/src/main/java/com/yihu/jw/manage/model/login/ManageLoginLog.java
+++ svr/svr-manage/src/main/java/com/yihu/jw/manage/model/login/ManageLoginLog.java
@@ -1,23 +1,22 @@
 package com.yihu.jw.manage.model.login;// default package
 
 import com.yihu.jw.manage.model.IdEntity;
+import org.hibernate.annotations.GenericGenerator;
 
 import java.sql.Timestamp;
 import java.util.Date;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 /**
  * ManageLoginLog entity. @author MyEclipse Persistence Tools
  */
 @Entity
 @Table(name = "manage_login_log")
-public class ManageLoginLog extends IdEntity implements java.io.Serializable {
+public class ManageLoginLog implements java.io.Serializable {
 	public static Integer type_error=0;
 	public static Integer type_success=1;
 	// Fields
-
+	private String id;
 	private Date loginTime;
 	private String loginAccount;
 	private String loginUser;
@@ -45,7 +44,16 @@ public class ManageLoginLog extends IdEntity implements java.io.Serializable {
 		this.type = type;
 		this.errorMessage = errorMessage;
 	}
+	@Id
+	@GeneratedValue(generator = "uuid")
+	@GenericGenerator(name = "uuid", strategy = "uuid")
+	public String getId() {
+		return id;
+	}
 
+	public void setId(String id) {
+		this.id = id;
+	}
 
 	@Column(name = "login_time", nullable = false, length = 0)
 	public Date getLoginTime() {
