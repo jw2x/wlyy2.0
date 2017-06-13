d0a571e2e67546e9fac99c9e6fb15f583d25cd4a
chenweida <494679975@qq.com>
2017-06-13 14:20:37 +0800

    用户模块提交

--- svr/svr-manage/src/main/resources/webapp/html/system/user/js/userList.js
+++ svr/svr-manage/src/main/resources/webapp/html/system/user/js/userList.js
@@ -1,29 +1,30 @@
 $(document).ready(function () {
     debugger
+    //初始化用户列表
     $("#userlist").dataTable({
         serverSide: true, //开启服务器模式
-        paging:true,//是否显示分页栏，默认是true
+        paging: true,//是否显示分页栏，默认是true
         searching: false, //禁用搜索和排序 默认false
-        ordering:  false,//禁止排序 默认false
+        ordering: false,//禁止排序 默认false
         ajax: {
             url: '/user/list',
             type: 'GET',
-            dataSrc:"detailModelList"
+            dataSrc: "detailModelList"
         },
         columns: [
-            { data: 'detailModelList.name' },
-            { data: 'detailModelList.code' },
-            { data: 'detailModelList.mobile' }
+            {data: 'detailModelList.name'},
+            {data: 'detailModelList.code'},
+            {data: 'detailModelList.mobile'}
         ],
         "columnDefs": [  //隐藏第几列  下标从0开始
             {
-                "targets": [ 0,2 ],
+                "targets": [0, 2],
                 "visible": false,
                 "searchable": false
             }
         ],
         "order": [[1, 'asc']]  //索引行
-});
-alert(5555)
-    //获取用户列表
+    });
+
+
 });
