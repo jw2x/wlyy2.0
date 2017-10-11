546662641a1e1c2157333eb1f6deb294882059f2
chenweida <494679975@qq.com>
2017-10-11 11:06:01 +0800

    新增native

--- server/svr-dashboard/src/main/java/com/yihu/jw/DashboardApplication.java
+++ server/svr-dashboard/src/main/java/com/yihu/jw/DashboardApplication.java
@@ -4,12 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
-import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
-import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
-import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 /**
  * Created by chenweida on 2017/5/16.
