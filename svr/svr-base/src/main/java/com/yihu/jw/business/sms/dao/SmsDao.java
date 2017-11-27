d884e43e5e5ee6f0de8334900c3bfc65536b6c9f
chenweida <494679975@qq.com>
2017-11-27 14:25:43 +0800

    代码重构

--- svr/svr-base/src/main/java/com/yihu/jw/business/sms/dao/SmsDao.java
+++ svr/svr-base/src/main/java/com/yihu/jw/business/sms/dao/SmsDao.java
@@ -1,9 +1,7 @@
 package com.yihu.jw.business.sms.dao;
 
-import com.fasterxml.jackson.databind.deser.Deserializers;
 import com.yihu.jw.base.sms.BaseSms;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
 public interface SmsDao extends PagingAndSortingRepository<BaseSms, String>, JpaSpecificationExecutor<BaseSms> {
