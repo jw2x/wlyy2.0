b573e512914e113532ddd3151f9d0d09e66fd079
chenweida <494679975@qq.com>
2017-12-08 13:52:09 +0800

    新增电话号码验证

--- svr/svr-demo/src/main/java/com/yihu/jw/service/ClientService.java
+++ svr/svr-demo/src/main/java/com/yihu/jw/service/ClientService.java
@@ -24,13 +24,19 @@ import java.util.Set;
  */
 @Component("baseClientDetailsService")
 public class ClientService implements ClientServiceProvider {
-
+    /**
+     * 根据自己的业务查询表 返回相关的平台用户信息
+     * @param clientId
+     * @return
+     * @throws ClientRegistrationException
+     */
     @Override
     public ClientDetails loadClientByClientId(String clientId) throws ClientRegistrationException {
 //        SaasDO saasDO = saasDao.findByAppId(clientId);
 //        if (saasDO == null) {
 //            throw new ClientRegistrationException("用户没有注册");
 //        }
+
         SaasDO baseClientDetails = new SaasDO();
         baseClientDetails.setAppId("cwd");
         baseClientDetails.setAppSecret("cwd");
@@ -38,6 +44,4 @@ public class ClientService implements ClientServiceProvider {
         baseClientDetails.getAuthorizedGrantTypes();
         return baseClientDetails;
     }
-
-
 }
