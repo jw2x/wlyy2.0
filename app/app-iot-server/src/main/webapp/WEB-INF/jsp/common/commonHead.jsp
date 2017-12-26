35390b185012c24a80f17df77c214d5dabb30290
chenweida <494679975@qq.com>
2017-12-26 14:19:57 +0800

    解决webapp映射问题

---
+++ app/app-iot-server/src/main/webapp/WEB-INF/jsp/common/commonHead.jsp
@@ -0,0 +1,21 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"  pageEncoding="utf-8"%>
+<%@include file="/WEB-INF/jsp/common/commonInclude.jsp" %>
+
+<%--定义页面文档类型以及使用的字符集,浏览器会根据此来调用相应的字符集显示页面内容--%>
+<meta http-equiv="Content-Type" content="text/html;charset=utf-8">
+
+<%--IE=edge告诉IE使用最新的引擎渲染网页，chrome=1则可以激活Chrome Frame.--%>
+<meta http-equiv="X-UA-Compatible" content="IE=Edge,chrome=1">
+
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<link rel="stylesheet" href="${staticRoot}/css/reset.css">
+<link rel="stylesheet" href="${staticRoot}/css/bootstrap.min.css">
+
+<head>
+    <link rel="icon" href="${staticRoot}/images/logo.png" type="image/x-icon" />
+    <link rel="shortcut icon" href="${staticRoot}/images/logo.png" type="image/x-icon" />
+</head>
+
+
+
---
+++ app/app-iot-server/src/main/webapp/front/js/scripts/login.js
@@ -0,0 +1,55 @@
+/**
+ * Created by JKZL-A on 2017/10/20.
+ */
+var loginRelyOn = ['jquery', 'layer', 'vue', 'jsHelper', 'apiServer', 'promise', 'bootstrap', 'validate'];
+require(loginRelyOn, function ($, layer, vue, jsHelper, apiServer, Promise) {
+   jsHelper.setLayerCon();
+    var load = null;
+    var loginApp = new vue({
+        el: '#loginApp',
+        data: {
+            userID: '',
+            password: ''
+        },
+        methods: {
+            setLoad: function () {
+                load =layer.load(1, {
+                    shade: [0.5,'#000']
+                });
+            },
+            sendFun: function () {
+                if (this.userID == '') {
+                    this.tips('请输入账号', '#lUAn');
+                    return ;
+                }
+                if (this.password == '') {
+                    this.tips('请输入密码', '#lUPwd');
+                    return ;
+                }
+                this.setLoad();
+                apiServer.login({
+                    data:{
+                        "userName": this.userID,
+                        "password": this.password
+                    }
+                }).then(function (res) {
+                    layer.close(load);
+                    if (res.successFlg) {
+                        sessionStorage.setItem("ZF_UID",res.data.user.id);
+                        sessionStorage.setItem("ZF_UN",res.data.user.realName);
+                        sessionStorage.setItem("ZF_U",res.data.user);
+                        location.href = apiServer.indexPage();
+                    } else {
+                        layer.msg(res.message);
+                    }
+                });
+            },
+            tips: function (msg, id) {
+                layer.tips(msg, id,{
+                    tips: [2, '#ff5722'],
+                    time: 1000
+                });
+            }
+        }
+    });
+});
