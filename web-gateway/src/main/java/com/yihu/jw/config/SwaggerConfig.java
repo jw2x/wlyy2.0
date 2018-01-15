9bb8c8be5e02596bb2fdb7db93cf935a60036fe3
yeshijie <707081346@qq.com>
2018-01-15 09:17:47 +0800

    物联网相关

--- web-gateway/src/main/java/com/yihu/jw/config/SwaggerConfig.java
+++ web-gateway/src/main/java/com/yihu/jw/config/SwaggerConfig.java
@@ -1,8 +1,5 @@
 package com.yihu.jw.config;
 
-import com.yihu.jw.commnon.base.base.BaseContants;
-import com.yihu.jw.commnon.base.wx.WechatContants;
-import com.yihu.jw.commnon.wlyy.AgreementContants;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -13,9 +10,6 @@ import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-import static com.google.common.base.Predicates.or;
-import static springfox.documentation.builders.PathSelectors.regex;
-
 @Configuration
 @EnableSwagger2
 @ComponentScan("com.yihu.jw.controller.**")
@@ -23,6 +17,7 @@ public class SwaggerConfig {
     public static final String base_API = "base";
     public static final String wlyy_API = "wlyy";
     public static final String login_API = "login";
+    public static final String iot_API = "iot";
 
 
     @Bean
@@ -95,6 +90,32 @@ public class SwaggerConfig {
     private ApiInfo loginApiInfo() {
         ApiInfo apiInfo = new ApiInfo("基卫2.0API",
                 "基卫2.0API，提供基础卫生相关服务。",
+                "1.0",
+                "No terms of service",
+                "wenfujian@jkzl.com",
+                "The Apache License, Version 2.0",
+                "http://www.apache.org/licenses/LICENSE-2.0.html"
+        );
+
+        return apiInfo;
+    }
+
+    @Bean
+    public Docket iotAPI() {
+
+        return new Docket(DocumentationType.SWAGGER_2)
+                .groupName(iot_API)
+                .useDefaultResponseMessages(false)
+                .apiInfo(iotApiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.yihu.jw.controller.iot"))
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    private ApiInfo iotApiInfo() {
+        ApiInfo apiInfo = new ApiInfo("基卫2.0API",
+                "基卫2.0API，提供物联网相关服务。",
                 "1.0",
                 "No terms of service",
                 "wenfujian@jkzl.com",
