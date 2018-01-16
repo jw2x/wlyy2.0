265e74638471f7fd1f1b9480f107b1e7c0910250
chenweida <494679975@qq.com>
2018-01-16 20:40:38 +0800

    解决webapp映射问题

---
+++ common/common-rest-model/src/main/java/com/yihu/jw/restmodel/common/base/BaseEnvelop.java
@@ -0,0 +1,46 @@
+package com.yihu.jw.restmodel.common.base;
+
+/**
+ * Created by chenweida on 2018/1/16.
+ */
+public class BaseEnvelop {
+
+    protected String errorCode;
+
+    protected String errorMsg;
+
+    protected String successMsg;
+
+    public BaseEnvelop() {
+    }
+
+    public BaseEnvelop(String errorCode, String errorMsg) {
+        this.errorCode = errorCode;
+        this.errorMsg = errorMsg;
+    }
+
+    public String getErrorMsg() {
+        return errorMsg;
+    }
+
+    public void setErrorMsg(String errorMsg) {
+        this.errorMsg = errorMsg;
+    }
+
+    public String getSuccessMsg() {
+        return successMsg;
+    }
+
+    public void setSuccessMsg(String successMsg) {
+        this.successMsg = successMsg;
+    }
+
+    public String getErrorCode() {
+        return errorCode;
+    }
+
+    public void setErrorCode(String errorCode) {
+        this.errorCode = errorCode;
+    }
+
+}
