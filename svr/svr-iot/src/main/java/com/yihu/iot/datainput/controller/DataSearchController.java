26f52dbb7fbdb7569a0c8a3d95fba24834c03957
LiTaohong <1026980510@qq.com>
2018-01-25 19:33:46 +0800

    更新标记纠正

--- svr/svr-iot/src/main/java/com/yihu/iot/datainput/controller/DataSearchController.java
+++ svr/svr-iot/src/main/java/com/yihu/iot/datainput/controller/DataSearchController.java
@@ -118,7 +118,7 @@ public class DataSearchController {
     public Envelop update(@ApiParam(name = "json_data", value = "", defaultValue = "") @RequestBody String jsonData){
         try{
             String str = dataSearchService.updateData(jsonData);
-            if(!StringUtils.equalsIgnoreCase("success",str)){
+            if(!StringUtils.equalsIgnoreCase("true",str)){
                 return Envelop.getSuccess(DataRequestMapping.DataSearch.message_fail,str);
             }
             return Envelop.getSuccess(DataRequestMapping.DataSearch.update_success,str);
