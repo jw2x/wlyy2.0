7125359398f8a5c8357c2ed22ffa8f2355b7cb9c
chenweida <494679975@qq.com>
2018-02-22 09:41:21 +0800

    pom文件修改

--- base/common-security/src/main/java/com.yihu.base.security/rbas/ClientServiceProvider.java
+++
@@ -1,9 +0,0 @@
-package com.yihu.base.security.rbas;
-
-import org.springframework.security.oauth2.provider.ClientDetailsService;
-
-/**
- * Created by chenweida on 2017/12/5.
- */
-public interface ClientServiceProvider extends ClientDetailsService {
-}
