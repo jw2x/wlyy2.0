4940502e13eec760d9638616ca26c23caac4a9fd
trick9191 <350611167@qq.com>
2018-04-24 15:16:39 +0800

    专科医生

---
+++ common/common-entity/src/main/java/com/yihu/jw/wlyy/specialist/SpecialistDo.java
@@ -0,0 +1,98 @@
+package com.yihu.jw.wlyy.specialist;
+
+import com.yihu.jw.IdEntityWithOperation;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import java.io.Serializable;
+
+/**
+ * Created by Trick on 2018/4/24.
+ */
+@Entity
+@Table(name = "wlyy_specialist")
+public class SpecialistDO extends IdEntityWithOperation implements Serializable {
+
+    private String name; //专科医生姓名
+    private String profession; //专业
+    private String professionName; // 专业名称
+    private String dept; //科室
+    private String deptName; //科室名称
+    private String hospital;//医院
+    private String hospitalName;//医院名称
+    private String del;//1：有效；0：删除
+
+    @Column(name = "name")
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Column(name = "profession")
+    public String getProfession() {
+        return profession;
+    }
+
+    public void setProfession(String profession) {
+        this.profession = profession;
+    }
+
+    @Column(name = "profession_name")
+    public String getProfessionName() {
+        return professionName;
+    }
+
+
+    public void setProfessionName(String professionName) {
+        this.professionName = professionName;
+    }
+
+    @Column(name = "dept")
+    public String getDept() {
+        return dept;
+    }
+
+    public void setDept(String dept) {
+        this.dept = dept;
+    }
+
+    @Column(name = "dept_name")
+    public String getDeptName() {
+        return deptName;
+    }
+
+    public void setDeptName(String deptName) {
+        this.deptName = deptName;
+    }
+
+    @Column(name = "hospital")
+    public String getHospital() {
+        return hospital;
+    }
+
+    public void setHospital(String hospital) {
+        this.hospital = hospital;
+    }
+
+    @Column(name = "hospital_name")
+    public String getHospitalName() {
+        return hospitalName;
+    }
+
+    public void setHospitalName(String hospitalName) {
+        this.hospitalName = hospitalName;
+    }
+
+    @Column(name = "del")
+    public String getDel() {
+        return del;
+    }
+
+    public void setDel(String del) {
+        this.del = del;
+    }
+}
