8d27a946c464f5d0ae390be980a245c16f6a508c
chenweida <494679975@qq.com>
2018-05-08 16:36:43 +0800

    新增状态

--- common/common-rest-model/src/main/java/com/yihu/jw/restmodel/common/base/BaseEnvelopStatus.java
+++ common/common-rest-model/src/main/java/com/yihu/jw/restmodel/common/base/BaseEnvelopStatus.java
@@ -5,11 +5,14 @@ package com.yihu.jw.restmodel.common.base;
  */
 public enum BaseEnvelopStatus {
     success("请求成功", 200),
+    //------------------登陆 权限相关 start ------------------
     status_10100("账号不存在", 10100),
     status_10101("密码错误", 10101),
     status_10102("用户未登录", 10102),
     status_10103("登陆超时", 10103),
-    status_10104("账号被挤", 10104),;
+    status_10104("账号被挤", 10104),
+    status_10105("账号没权限", 10105),;
+    //------------------登陆 权限相关 end ------------------
 
     BaseEnvelopStatus(String name, Integer code) {
         this.name = name;
