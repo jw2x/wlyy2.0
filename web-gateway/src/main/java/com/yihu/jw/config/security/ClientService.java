a370dfc345fca32b9ad3e5e2557d14601d8a6764
chenweida <494679975@qq.com>
2018-05-31 12:34:26 +0800

    修改继承关系

--- web-gateway/src/main/java/com/yihu/jw/config/security/ClientService.java
+++ web-gateway/src/main/java/com/yihu/jw/config/security/ClientService.java
@@ -1,56 +1,56 @@
-//package com.yihu.jw.config.security;
-//
-//import com.yihu.base.security.rbas.ClientServiceProvider;
-//import com.yihu.jw.base.base.SaasDO;
-//import com.yihu.jw.fegin.base.base.SaasFeign;
-//import com.yihu.jw.restmodel.common.Envelop;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.context.annotation.Primary;
-//import org.springframework.security.crypto.codec.Base64;
-//import org.springframework.security.oauth2.provider.ClientDetails;
-//import org.springframework.security.oauth2.provider.ClientRegistrationException;
-//import org.springframework.security.oauth2.provider.client.BaseClientDetails;
-//import org.springframework.stereotype.Component;
-//
-//import java.io.UnsupportedEncodingException;
-//import java.util.HashSet;
-//import java.util.Map;
-//import java.util.Set;
-//
-///**
-// * Created by chenweida on 2017/12/1.
-// */
-//@Component("baseClientDetailsService")
-//@Primary
-//public class ClientService implements ClientServiceProvider {
-//
-//    @Autowired
-//    private SaasFeign saasFeign;
-//
-//    @Override
-//    public ClientDetails loadClientByClientId(String clientId) throws ClientRegistrationException {
-//        Envelop envelop = saasFeign.findByCode(clientId);
-//        Map saasDO = (Map)envelop.getObj();
-//        if (saasDO.isEmpty()) {
-//            throw new ClientRegistrationException("该平台未注册");
-//        }
-//        BaseClientDetails baseClientDetails = new BaseClientDetails();
-//        baseClientDetails.setClientId(saasDO.get("id")+"");
-//        baseClientDetails.setClientSecret("");
-//
-//        Set<String> strings = new HashSet<>();
-//        strings.add("password");
-//        strings.add("custom_password");
-//        strings.add("authorization_code");
-//        strings.add("refresh_token");
-//        baseClientDetails.setAuthorizedGrantTypes(strings);
-//        return baseClientDetails;
+package com.yihu.jw.config.security;
+
+import com.yihu.base.security.rbas.ClientServiceProvider;
+import com.yihu.jw.base.base.SaasDO;
+import com.yihu.jw.fegin.base.base.SaasFeign;
+import com.yihu.jw.restmodel.common.Envelop;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Primary;
+import org.springframework.security.crypto.codec.Base64;
+import org.springframework.security.oauth2.provider.ClientDetails;
+import org.springframework.security.oauth2.provider.ClientRegistrationException;
+import org.springframework.security.oauth2.provider.client.BaseClientDetails;
+import org.springframework.stereotype.Component;
+
+import java.io.UnsupportedEncodingException;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * Created by chenweida on 2017/12/1.
+ */
+@Component("baseClientDetailsService")
+@Primary
+public class ClientService implements ClientServiceProvider {
+
+    @Autowired
+    private SaasFeign saasFeign;
+
+    @Override
+    public ClientDetails loadClientByClientId(String clientId) throws ClientRegistrationException {
+        Envelop envelop = saasFeign.findByCode(clientId);
+        Map saasDO = (Map)envelop.getObj();
+        if (saasDO.isEmpty()) {
+            throw new ClientRegistrationException("该平台未注册");
+        }
+        BaseClientDetails baseClientDetails = new BaseClientDetails();
+        baseClientDetails.setClientId(saasDO.get("id")+"");
+        baseClientDetails.setClientSecret("");
+
+        Set<String> strings = new HashSet<>();
+        strings.add("password");
+        strings.add("custom_password");
+        strings.add("authorization_code");
+        strings.add("refresh_token");
+        baseClientDetails.setAuthorizedGrantTypes(strings);
+        return baseClientDetails;
+    }
+
+//    public static void main(String[] args) {
+//        byte[] a = Base64.encode("1:".getBytes());
+//        String s = new String(a);
+//        System.out.println(s);
 //    }
-//
-////    public static void main(String[] args) {
-////        byte[] a = Base64.encode("1:".getBytes());
-////        String s = new String(a);
-////        System.out.println(s);
-////    }
-//
-//}
+
+}
--- web-gateway/src/main/java/com/yihu/jw/config/security/UserService.java
+++ web-gateway/src/main/java/com/yihu/jw/config/security/UserService.java
@@ -1,77 +1,77 @@
-//package com.yihu.jw.config.security;
-//
-//
-//import com.yihu.jw.base.user.BaseEmployDO;
-//import com.yihu.jw.fegin.base.user.BaseRoleFeign;
-//import com.yihu.jw.fegin.base.user.EmployFeign;
-//import com.yihu.jw.restmodel.common.Envelop;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.security.core.authority.AuthorityUtils;
-//import org.springframework.security.core.userdetails.User;
-//import org.springframework.security.core.userdetails.UserDetails;
-//import org.springframework.security.core.userdetails.UserDetailsService;
-//import org.springframework.security.core.userdetails.UsernameNotFoundException;
-//import org.springframework.security.crypto.password.PasswordEncoder;
-//import org.springframework.stereotype.Component;
-//import org.springframework.util.StringUtils;
-//
-//import java.util.List;
-//import java.util.Map;
-//
-///**
-// * Created by chenweida on 2017/11/29.
-// * 处理用户校验
-// */
-//@Component
-//public class UserService implements UserDetailsService {
-//
-//    @Autowired
-//    private PasswordEncoder passwordEncoder;
+package com.yihu.jw.config.security;
+
+
+import com.yihu.jw.base.user.BaseEmployDO;
+import com.yihu.jw.fegin.base.user.BaseRoleFeign;
+import com.yihu.jw.fegin.base.user.EmployFeign;
+import com.yihu.jw.restmodel.common.Envelop;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by chenweida on 2017/11/29.
+ * 处理用户校验
+ */
+@Component
+public class UserService implements UserDetailsService {
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+    @Autowired
+    private EmployFeign employFeign;
+    @Autowired
+    private BaseRoleFeign baseRoleFeign;
+
 //    @Autowired
 //    private EmployFeign employFeign;
-//    @Autowired
-//    private BaseRoleFeign baseRoleFeign;
-//
-////    @Autowired
-////    private EmployFeign employFeign;
-//
-//    /**
-//     * 我们只需要把用户返回给spring-security 密码框架自己帮我们校验
-//     *
-//     * @param saasPhone
-//     * @return
-//     * @throws UsernameNotFoundException
-//     */
-//    @Override
-//    public UserDetails loadUserByUsername(String saasPhone) throws UsernameNotFoundException {
-//
-//        //saasPhone=saas+phone
-//        //根据userName获取账户信息（手机号码）
-//        String[] sp = saasPhone.split(",");
-//        String phone = sp[0];
-//        String saasId = sp[1];
-//        Envelop envelop = employFeign.getEmployeeByPhoneAndSaasId(phone,saasId);
-//        if (envelop!=null&&!((Map)envelop.getObj()).isEmpty()) {
-//            Map baseEmployDO =  (Map)envelop.getObj();
-//            Envelop roles =  baseRoleFeign.getPhoneAndSaasId(phone,saasId);
-//            String authority ="";
-//            if(roles!=null&&!((List<Map>)roles.getObj()).isEmpty()){
-//                List<Map> list =  (List<Map>)roles.getObj();
-//                for(Map one :list){
-//                    authority+=","+one.get("code");
-//                }
-//            }
-//            return new User(saasPhone,
-//                    passwordEncoder.encode(baseEmployDO.get("password")+""),
-//                    true,
-//                    true,
-//                    true,
-//                    true
-//                    , AuthorityUtils.commaSeparatedStringToAuthorityList(authority.startsWith(",")?authority.substring(1):"") //权限
-//            );
-//
-//        } else {
-//            throw new UsernameNotFoundException("用户不存在");
-//        }
-//    }
-//}
+
+    /**
+     * 我们只需要把用户返回给spring-security 密码框架自己帮我们校验
+     *
+     * @param saasPhone
+     * @return
+     * @throws UsernameNotFoundException
+     */
+    @Override
+    public UserDetails loadUserByUsername(String saasPhone) throws UsernameNotFoundException {
+
+        //saasPhone=saas+phone
+        //根据userName获取账户信息（手机号码）
+        String[] sp = saasPhone.split(",");
+        String phone = sp[0];
+        String saasId = sp[1];
+        Envelop envelop = employFeign.getEmployeeByPhoneAndSaasId(phone,saasId);
+        if (envelop!=null&&!((Map)envelop.getObj()).isEmpty()) {
+            Map baseEmployDO =  (Map)envelop.getObj();
+            Envelop roles =  baseRoleFeign.getPhoneAndSaasId(phone,saasId);
+            String authority ="";
+            if(roles!=null&&!((List<Map>)roles.getObj()).isEmpty()){
+                List<Map> list =  (List<Map>)roles.getObj();
+                for(Map one :list){
+                    authority+=","+one.get("code");
+                }
+            }
+            return new User(saasPhone,
+                    passwordEncoder.encode(baseEmployDO.get("password")+""),
+                    true,
+                    true,
+                    true,
+                    true
+                    , AuthorityUtils.commaSeparatedStringToAuthorityList(authority.startsWith(",")?authority.substring(1):"") //权限
+            );
+
+        } else {
+            throw new UsernameNotFoundException("用户不存在");
+        }
+    }
+}
