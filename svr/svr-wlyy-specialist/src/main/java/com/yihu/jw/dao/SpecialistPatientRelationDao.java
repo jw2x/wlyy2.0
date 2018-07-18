01559890690801f0323261b24809f0188cf6e228
trick9191 <350611167@qq.com>
2018-07-18 16:19:05 +0800

    专科医生

--- svr/svr-wlyy-specialist/src/main/java/com/yihu/jw/dao/SpecialistPatientRelationDao.java
+++ svr/svr-wlyy-specialist/src/main/java/com/yihu/jw/dao/SpecialistPatientRelationDao.java
@@ -2,6 +2,7 @@ package com.yihu.jw.dao;
 
 import com.yihu.jw.entity.specialist.SpecialistPatientRelationDO;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
 import java.util.List;
@@ -13,5 +14,6 @@ public interface SpecialistPatientRelationDao extends PagingAndSortingRepository
         JpaSpecificationExecutor<SpecialistPatientRelationDO> {
     public List<SpecialistPatientRelationDO> findByDoctorAndStatus(String doctor,String status);
 
+    @Query("select p from SpecialistPatientRelationDO p where p.doctor=?1 and p.patient =?2 and p.status>=0")
     public SpecialistPatientRelationDO findByDoctorAndPatient(String doctor,String patient);
 }
