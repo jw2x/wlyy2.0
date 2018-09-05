e5b38716280fb605bd45b48a71b2bf7494d8c3a8
trick9191 <350611167@qq.com>
2018-09-05 14:20:29 +0800

    通用构件

---
+++ svr/svr-base/src/main/java/com/yihu/jw/base/dao/wx/WxAccessTokenDao.java
@@ -0,0 +1,17 @@
+package com.yihu.jw.base.dao.wx;
+
+import com.yihu.jw.entity.base.wx.WxAccessTokenDO;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+import java.util.List;
+
+/**
+ * Created by Administrator on 2017/5/18 0018.
+ */
+public interface WxAccessTokenDao extends PagingAndSortingRepository<WxAccessTokenDO, String>, JpaSpecificationExecutor<WxAccessTokenDO> {
+
+    @Query("from WxAccessTokenDO w where w.wechatId =?1 order by w.addTimestamp desc")
+    List<WxAccessTokenDO> getWxAccessTokenById(String wechatId);
+}
---
+++ svr/svr-base/src/main/java/com/yihu/jw/base/dao/wx/WxGraphicSceneGroupDao.java
@@ -0,0 +1,15 @@
+package com.yihu.jw.base.dao.wx;
+
+import com.yihu.jw.entity.base.wx.WxGraphicSceneGroupDO;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+import java.util.List;
+
+/**
+ * Created by Trick on 2018/8/16.
+ */
+public interface WxGraphicSceneGroupDao extends PagingAndSortingRepository<WxGraphicSceneGroupDO, String>, JpaSpecificationExecutor<WxGraphicSceneGroupDO> {
+
+    List<WxGraphicSceneGroupDO> findByWechatIdAndScene(String wechatId, String scene);
+}
