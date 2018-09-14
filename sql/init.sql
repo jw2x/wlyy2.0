e8c0bb23b8dee958bba012dc1c78ca823a33ac12
suxiaoyang <653800209@qq.com>
2018-09-14 10:19:04 +0800

    完善

--- sql/init.sql
+++ sql/init.sql
@@ -408,3 +408,276 @@ create table `dict_hospital_dept`
   primary key (id)
 )
   ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='医院科室字典';
+
+-- 模块
+Drop table IF EXISTS `base_module`;
+CREATE TABLE `base_module` (
+  `id` varchar(50) CHARACTER SET utf8 NOT NULL COMMENT '模块id',
+  `name` varchar(200) COLLATE utf8_unicode_ci NOT NULL COMMENT '名称',
+  `parent_id` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL COMMENT '父业务id',
+  `status` int(11) NOT NULL COMMENT '状态（0-不可用，1-可用，2-删除）',
+  `create_user` varchar(200) COLLATE utf8_unicode_ci DEFAULT NULL COMMENT '创建人',
+  `create_user_name` varchar(200) COLLATE utf8_unicode_ci DEFAULT NULL COMMENT '创建人名',
+  `create_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `update_user` varchar(200) COLLATE utf8_unicode_ci DEFAULT NULL COMMENT '修改人',
+  `update_user_name` varchar(200) COLLATE utf8_unicode_ci DEFAULT NULL COMMENT '修改人名',
+  `update_time` datetime NOT NULL COMMENT '修改时间',
+  `remark` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL COMMENT '备注',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci COMMENT='模块';
+
+-- 功能
+Drop table IF EXISTS `base_function`;
+CREATE TABLE `base_function` (
+  `id` varchar(50) CHARACTER SET utf8 NOT NULL COMMENT '功能id',
+  `module_id` varchar(50) CHARACTER SET utf8 NOT NULL COMMENT '模块ID',
+  `name` varchar(200) COLLATE utf8_unicode_ci NOT NULL COMMENT '功能名称',
+  `prefix` varchar(255) CHARACTER SET utf8 DEFAULT NULL COMMENT '使用网关转发时的url前缀',
+  `url` varchar(255) COLLATE utf8_unicode_ci NOT NULL COMMENT '功能对应的后台url',
+  `create_user` varchar(200) COLLATE utf8_unicode_ci DEFAULT NULL COMMENT '创建人',
+  `create_user_name` varchar(200) COLLATE utf8_unicode_ci DEFAULT NULL COMMENT '创建人名',
+  `create_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `update_user` varchar(200) COLLATE utf8_unicode_ci DEFAULT NULL COMMENT '修改人',
+  `update_user_name` varchar(200) COLLATE utf8_unicode_ci DEFAULT NULL COMMENT '修改人名',
+  `update_time` datetime NOT NULL DEFAULT '0000-00-00 00:00:00' COMMENT '修改时间',
+  `remark` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL COMMENT '备注',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci COMMENT='功能';
+
+-- SAAS
+Drop table IF EXISTS `base_saas`;
+CREATE TABLE `base_saas` (
+  `id` varchar(255) COLLATE utf8_unicode_ci NOT NULL COMMENT 'saas业务标识',
+  `create_time` datetime NOT NULL COMMENT '创建时间',
+  `create_user` varchar(50) COLLATE utf8_unicode_ci DEFAULT NULL COMMENT '创建者',
+  `create_user_name` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL COMMENT '创建者',
+  `update_time` datetime NOT NULL COMMENT '更新时间',
+  `update_user` varchar(50) COLLATE utf8_unicode_ci DEFAULT NULL COMMENT '更新者',
+  `update_user_name` datetime DEFAULT NULL COMMENT '更新者',
+  `org_code` varchar(50) COLLATE utf8_unicode_ci NOT NULL COMMENT '机构编码',
+  `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL COMMENT '系统名称',
+  `status` int(11) NOT NULL COMMENT '状态（0-待审核 1-审核通过 2-审核不通过 3-已删除）',
+  `remark` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL COMMENT '备注',
+  `logo` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL COMMENT '远程fastDFS文件地址',
+  `type` int(11) NOT NULL COMMENT '类型',
+  `manager` varchar(50) COLLATE utf8_unicode_ci NOT NULL COMMENT '管理员 - 关联user表id字段',
+  `email` varchar(255) COLLATE utf8_unicode_ci NOT NULL COMMENT '管理员邮箱',
+  `mobile` varchar(255) COLLATE utf8_unicode_ci NOT NULL COMMENT '管理员手机号码',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci COMMENT='SAAS';
+
+-- Saas模块功能
+Drop table IF EXISTS `base_saas_module_function`;
+CREATE TABLE `base_saas_module_function` (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '主键',
+  `saas_id` varchar(50) CHARACTER SET utf8 NOT NULL COMMENT '角色ID',
+  `module_id` varchar(50) CHARACTER SET utf8 NOT NULL COMMENT '模块ID',
+  `function_id` varchar(50) CHARACTER SET utf8 NOT NULL COMMENT '功能ID',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci COMMENT='Saas模块功能';
+
+-- Saas类型默认的模块功能
+Drop table IF EXISTS `base_saas_default_module_function`;
+CREATE TABLE `base_saas_default_module_function` (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '主键',
+  `saas_type` int(11) NOT NULL COMMENT 'saas类型',
+  `module_id` varchar(50) CHARACTER SET utf8 NOT NULL COMMENT '模块ID',
+  `function_id` varchar(50) CHARACTER SET utf8 NOT NULL COMMENT '功能ID',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci COMMENT='Saas类型默认的模块功能';
+
+-- Saas名片
+Drop table IF EXISTS `base_saas_business_card`;
+CREATE TABLE `base_saas_business_card` (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '自增主键',
+  `saas_id` varchar(50) CHARACTER SET utf8 NOT NULL COMMENT 'SAAS ID',
+  `field` varchar(255) CHARACTER SET utf8 NOT NULL COMMENT '所要生成的目标名片的字段，如医生的name、age等',
+  `type` int(11) NOT NULL COMMENT '名片类型 （0 - 医生， 1-居民）',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci COMMENT='Saas名片';
+
+-- 短信网关
+Drop table IF EXISTS `base_sms_gateway`;
+CREATE TABLE `base_sms_gateway` (
+  `id` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `client_id` varchar(10) COLLATE utf8_unicode_ci DEFAULT NULL COMMENT 'saas_id 关联 base_saas',
+  `name` varchar(255) CHARACTER SET utf8 DEFAULT NULL COMMENT '网关名称',
+  `username` varchar(50) COLLATE utf8_unicode_ci DEFAULT NULL COMMENT '网关用户名',
+  `password` varchar(50) COLLATE utf8_unicode_ci DEFAULT NULL COMMENT '网关密码',
+  `website` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL COMMENT '官网地址',
+  `expire_min` int(11) NOT NULL COMMENT '超时时间（分钟）',
+  `request_url` varchar(255) COLLATE utf8_unicode_ci NOT NULL COMMENT '网关接口访问URL',
+  `request_certificate` varchar(2550) CHARACTER SET utf8 NOT NULL COMMENT '调用凭证（json串）',
+  `response_code` varchar(255) CHARACTER SET utf8 NOT NULL COMMENT '短信接口调用状态标识',
+  `success_value` varchar(255) COLLATE utf8_unicode_ci NOT NULL COMMENT '调用成功的状态码',
+  `status` int(11) NOT NULL COMMENT '0-禁用，1-可用，2-不可用',
+  `create_user` varchar(50) CHARACTER SET utf8 DEFAULT NULL COMMENT '创建人',
+  `create_user_name` varchar(255) CHARACTER SET utf8 DEFAULT NULL COMMENT '创建人名',
+  `create_time` datetime NOT NULL COMMENT '创建时间',
+  `update_user` varchar(50) CHARACTER SET utf8 DEFAULT NULL COMMENT '修改人',
+  `update_user_name` varchar(255) CHARACTER SET utf8 DEFAULT NULL COMMENT '修改人名',
+  `update_time` datetime NOT NULL COMMENT '修改时间',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci COMMENT='短信网关';
+
+-- 短信模板
+Drop table IF EXISTS `base_sms_template`;
+CREATE TABLE `base_sms_template` (
+  `id` varchar(50) CHARACTER SET utf8 NOT NULL COMMENT '主键',
+  `client_id` varchar(10) CHARACTER SET utf8 NOT NULL COMMENT '应用ID',
+  `type` int(11) NOT NULL COMMENT '短信标签',
+  `header` varchar(255) CHARACTER SET utf8 NOT NULL COMMENT '【短信抬头】',
+  `content` varchar(255) CHARACTER SET utf8 NOT NULL COMMENT '正文',
+  `create_user` varchar(50) CHARACTER SET utf8 DEFAULT NULL COMMENT '创建人',
+  `create_user_name` varchar(255) CHARACTER SET utf8 DEFAULT NULL COMMENT '创建人名',
+  `create_time` datetime NOT NULL COMMENT '创建时间',
+  `update_user` varchar(50) CHARACTER SET utf8 DEFAULT NULL COMMENT '修改人',
+  `update_user_name` varchar(255) CHARACTER SET utf8 DEFAULT NULL COMMENT '修改人名',
+  `update_time` datetime NOT NULL COMMENT '修改时间',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci COMMENT='短信模板';
+
+-- 短信记录
+Drop table IF EXISTS `base_sms`;
+CREATE TABLE `base_sms` (
+  `id` varchar(50) COLLATE utf8_unicode_ci NOT NULL COMMENT '业务无关主键',
+  `client_id` varchar(10) COLLATE utf8_unicode_ci NOT NULL COMMENT '应用ID',
+  `sms_gateway_id` varchar(50) CHARACTER SET utf8 NOT NULL COMMENT '使用的网关ID',
+  `request_ip` varchar(255) CHARACTER SET utf8 NOT NULL COMMENT '请求的ip地址',
+  `mobile` varchar(50) COLLATE utf8_unicode_ci NOT NULL COMMENT '接收手机号',
+  `content` varchar(500) COLLATE utf8_unicode_ci NOT NULL COMMENT '消息内容',
+  `deadline` datetime NOT NULL COMMENT '过期时间',
+  `captcha` varchar(50) COLLATE utf8_unicode_ci NOT NULL COMMENT '验证码',
+  `type` int(11) NOT NULL COMMENT '消息类型：1微信端注册，2微信端找回密码，3医生端找回密码，4患者登录，5医生登录',
+  `create_user` varchar(50) CHARACTER SET utf8 DEFAULT NULL COMMENT '创建人',
+  `create_user_name` varchar(255) CHARACTER SET utf8 DEFAULT NULL COMMENT '创建人名',
+  `create_time` datetime NOT NULL COMMENT '创建时间',
+  `update_user` varchar(50) CHARACTER SET utf8 DEFAULT NULL COMMENT '修改人',
+  `update_user_name` varchar(255) CHARACTER SET utf8 DEFAULT NULL COMMENT '修改人名',
+  `update_time` datetime NOT NULL COMMENT '修改时间',
+  PRIMARY KEY (`id`),
+  KEY `idx_wlyy_sms_1` (`mobile`),
+  KEY `idx_wlyy_sms_2` (`client_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci COMMENT='短信记录';
+
+-- 系统字典
+Drop table IF EXISTS `base_system_dict`;
+CREATE TABLE `base_system_dict` (
+  `saas_id` varchar(50) DEFAULT NULL COMMENT 'SaasId',
+  `code` varchar(50) NOT NULL COMMENT '字典代码',
+  `py_code` varchar(50) NOT NULL COMMENT '拼音码',
+  `name` varchar(50) NOT NULL COMMENT '字典名称',
+  `type` int(11) NOT NULL COMMENT '字典类型（0-基础字典，1-扩展字典）',
+  PRIMARY KEY (`code`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='系统字典';
+
+-- 系统字典项
+Drop table IF EXISTS `base_system_dict_entry`;
+CREATE TABLE `base_system_dict_entry` (
+  `id` varchar(50) CHARACTER SET utf8 NOT NULL COMMENT '主键',
+  `dict_code` varchar(50) COLLATE utf8_unicode_ci NOT NULL COMMENT '所属字典代码',
+  `code` varchar(50) COLLATE utf8_unicode_ci NOT NULL COMMENT '字典项代码',
+  `py_code` varchar(50) COLLATE utf8_unicode_ci NOT NULL COMMENT '字典项拼音码',
+  `value` varchar(255) CHARACTER SET utf8 NOT NULL COMMENT '字典项值',
+  `sort` int(11) DEFAULT NULL,
+  `remark` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL COMMENT '说明',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci COMMENT='系统字典项';
+
+-- 后台管理员
+Drop table IF EXISTS `base_user`;
+CREATE TABLE `base_user` (
+  `id` varchar(50) CHARACTER SET utf8 NOT NULL COMMENT '主键',
+  `create_time` datetime NOT NULL COMMENT '创建时间',
+  `create_user` varchar(50) CHARACTER SET utf8 DEFAULT NULL COMMENT '创建用户的ID',
+  `create_user_name` varchar(255) CHARACTER SET utf8 DEFAULT NULL COMMENT '创建用户的用户名',
+  `update_time` datetime NOT NULL COMMENT '更新时间',
+  `update_user` varchar(50) CHARACTER SET utf8 DEFAULT NULL COMMENT '更新用户的ID',
+  `update_user_name` varchar(255) CHARACTER SET utf8 DEFAULT NULL COMMENT '更新用户的用户名',
+  `username` varchar(255) CHARACTER SET utf8 NOT NULL COMMENT '用户名',
+  `password` varchar(255) CHARACTER SET utf8 NOT NULL COMMENT '密码',
+  `salt` varchar(255) CHARACTER SET utf8 NOT NULL COMMENT '密码散列值',
+  `name` varchar(255) CHARACTER SET utf8 NOT NULL COMMENT '真实名字',
+  `gender` int(11) DEFAULT NULL COMMENT '性别（0-男，1-女）',
+  `idcard` varchar(255) CHARACTER SET utf8 DEFAULT NULL COMMENT '身份证号码',
+  `mobile` varchar(255) CHARACTER SET utf8 DEFAULT NULL COMMENT '手机号码',
+  `ak` varchar(255) CHARACTER SET utf8 DEFAULT NULL COMMENT '简化模式获取token所需要的凭证',
+  `email` varchar(255) CHARACTER SET utf8 DEFAULT NULL COMMENT '邮箱',
+  `enabled` int(1) DEFAULT NULL COMMENT '账号是否可用1为可用',
+  `locked` int(1) DEFAULT NULL COMMENT '账号是否被锁',
+  `locked_date` datetime DEFAULT NULL COMMENT '账号被锁时间',
+  `login_date` datetime DEFAULT NULL COMMENT '最后登陆时间',
+  `login_failure_count` int(11) DEFAULT NULL COMMENT '用户名密码验证失败次数'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci COMMENT='后台管理员';
+
+-- 角色
+Drop table IF EXISTS `base_role`;
+CREATE TABLE `base_role` (
+  `id` varchar(50) NOT NULL COMMENT '主键',
+  `saas_id` varchar(50) DEFAULT NULL COMMENT 'saas_id',
+  `client_id` varchar(10) NOT NULL COMMENT '所属应用',
+  `name` varchar(50) DEFAULT NULL COMMENT '角色名称',
+  `code` varchar(16) NOT NULL COMMENT '角色代码',
+  `remark` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci DEFAULT NULL COMMENT '备注',
+  `create_user` varchar(50) CHARACTER SET utf8 COLLATE utf8_unicode_ci DEFAULT NULL COMMENT '创建人',
+  `create_user_name` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci DEFAULT NULL COMMENT '创建人名',
+  `create_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `update_user` varchar(50) CHARACTER SET utf8 COLLATE utf8_unicode_ci DEFAULT NULL COMMENT '修改人',
+  `update_user_name` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci DEFAULT NULL COMMENT '修改人名',
+  `update_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '修改时间',
+  `system` bit(1) NOT NULL COMMENT '是否系统角色',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `code` (`code`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='角色';
+
+-- 角色权限
+Drop table IF EXISTS `base_role_authority`;
+CREATE TABLE `base_role_authority` (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '自增主键，无关业务',
+  `role_id` varchar(50) CHARACTER SET utf8 NOT NULL COMMENT '角色ID',
+  `authorities` varchar(255) CHARACTER SET utf8 NOT NULL COMMENT '权限',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci COMMENT='角色权限';
+
+-- 用户角色
+Drop table IF EXISTS `base_user_role`;
+CREATE TABLE `base_user_role` (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '主键',
+  `user_id` varchar(50) CHARACTER SET utf8 NOT NULL COMMENT '用户ID',
+  `role_id` varchar(50) CHARACTER SET utf8 NOT NULL COMMENT '角色编码',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci COMMENT='用户角色';
+
+-- 基于MQ的消息推送
+Drop table IF EXISTS `base_mq_message`;
+CREATE TABLE `base_mq_message` (
+  `id` varchar(50) COLLATE utf8_unicode_ci NOT NULL COMMENT '主键',
+  `topic` varchar(255) CHARACTER SET utf8 NOT NULL COMMENT '主题',
+  `name` varchar(255) CHARACTER SET utf8 NOT NULL COMMENT '名称',
+  `remark` varchar(255) CHARACTER SET utf8 DEFAULT NULL COMMENT '备注',
+  `template` varchar(255) CHARACTER SET utf8 DEFAULT NULL COMMENT '模板',
+  `create_user` varchar(50) CHARACTER SET utf8 DEFAULT NULL COMMENT '创建人',
+  `create_user_name` varchar(255) CHARACTER SET utf8 DEFAULT NULL COMMENT '创建人名',
+  `create_time` datetime NOT NULL COMMENT '创建时间',
+  `update_user` varchar(50) CHARACTER SET utf8 DEFAULT NULL COMMENT '修改人',
+  `update_user_name` varchar(255) CHARACTER SET utf8 DEFAULT NULL COMMENT '修改人名',
+  `update_time` datetime NOT NULL COMMENT '修改时间',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci COMMENT='基于MQ的消息推送';
+
+-- 基于MQ的消息推送订阅者
+Drop table IF EXISTS `base_mq_message_subscriber`;
+CREATE TABLE `base_mq_message_subscriber` (
+  `id` varchar(50) CHARACTER SET utf8 NOT NULL COMMENT '主键',
+  `saas_id` varchar(50) CHARACTER SET utf8 NOT NULL COMMENT '所属SAAS',
+  `topic` varchar(255) CHARACTER SET utf8 NOT NULL COMMENT '主题',
+  `url` varchar(255) CHARACTER SET utf8 NOT NULL COMMENT '推送地址',
+  `remark` varchar(255) CHARACTER SET utf8 DEFAULT NULL COMMENT '备注',
+  `create_user` varchar(50) CHARACTER SET utf8 DEFAULT NULL COMMENT '创建人',
+  `create_user_name` varchar(255) CHARACTER SET utf8 DEFAULT NULL COMMENT '创建人名',
+  `create_time` datetime NOT NULL COMMENT '创建时间',
+  `update_user` varchar(50) CHARACTER SET utf8 DEFAULT NULL COMMENT '修改人',
+  `update_user_name` varchar(255) CHARACTER SET utf8 DEFAULT NULL COMMENT '修改人名',
+  `update_time` datetime NOT NULL COMMENT '修改时间'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci COMMENT='基于MQ的消息推送订阅者';
+
