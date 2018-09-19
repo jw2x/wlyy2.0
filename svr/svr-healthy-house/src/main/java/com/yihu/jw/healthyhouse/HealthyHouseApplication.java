1a1701b4a0a1d533464ce9348ae553d32ae86c45
huangzhiyong <944439284@qq.com>
2018-09-19 15:41:31 +0800

    健康小屋- 用户登录

--- svr/svr-healthy-house/src/main/java/com/yihu/jw/healthyhouse/HealthyHouseApplication.java
+++ svr/svr-healthy-house/src/main/java/com/yihu/jw/healthyhouse/HealthyHouseApplication.java
@@ -3,15 +3,25 @@ package com.yihu.jw.healthyhouse;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.oauth2.OAuth2AutoConfiguration;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.web.client.RestTemplate;
 
-@SpringBootApplication(exclude = OAuth2AutoConfiguration.class)
-@ComponentScan(basePackages = {"com"})
+@SpringBootApplication
+@ComponentScan(basePackages = {"com.yihu"})
 @EnableAspectJAutoProxy(proxyTargetClass = true, exposeProxy = true)
 public class HealthyHouseApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HealthyHouseApplication.class, args);
 	}
+
+	@Bean
+	@LoadBalanced
+	RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
+
 }
--- svr/svr-healthy-house/src/main/java/com/yihu/jw/healthyhouse/service/user/LoginService.java
+++ svr/svr-healthy-house/src/main/java/com/yihu/jw/healthyhouse/service/user/LoginService.java
@@ -6,10 +6,16 @@ import com.yihu.jw.healthyhouse.model.user.User;
 import com.yihu.jw.restmodel.wlyy.HouseUserContant;
 import com.yihu.jw.util.security.MD5;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.*;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.RestTemplate;
 
 import javax.servlet.http.HttpServletRequest;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -23,22 +29,71 @@ public class LoginService {
 
     @Autowired
     private UserService userService;
+    @Autowired
+    private RestTemplate restTemplate;
+    private String clientId;
     /**
-     * 登录
-     * @param username
+     *  手机验证码方式登录并自动注册
+     * @param loginCode
+     * @return
+     * @throws Exception
+     */
+    @Transactional(noRollbackForClassName = "ManageException")
+    public User phoneLogin(HttpServletRequest request, String loginCode) throws ManageException {
+        //判断用户信息是否存在
+        User user = userService.findByCode(loginCode);
+        if (user == null) {
+            //未注册用户进行账户注册
+            user = new User();
+            user.setLoginCode(loginCode);
+            user.setName(loginCode);
+            user.setTelephone(loginCode);
+            user.setCreator(loginCode);
+            user.setPassword(LoginInfo.DEFAULT_PASSWORD);
+            userService.saveOrUpdate(user,LoginInfo.SAVE_TYPE_PHONE);//设置默认密码123456
+        }else {
+            //已注册用户更改用户状态
+            user.setActivated(HouseUserContant.activated_active);
+            userService.saveOrUpdate(user,LoginInfo.SAVE_TYPE_PHONE);
+        }
+        request.getSession().setAttribute(LoginInfo.IS_LOGIN, true);
+        request.getSession().setAttribute(LoginInfo.TOKEN, ""); //TODO token是否添加
+        request.getSession().setAttribute(LoginInfo.LOGIN_NAME, user.getName());
+        request.getSession().setAttribute(LoginInfo.LOGIN_CODE, user.getLoginCode());
+        request.getSession().setAttribute(LoginInfo.USER_ID, user.getId());
+
+        return user;
+    }
+
+
+    /**
+     * i健康账户登录&注册
+     * @param loginCode
      * @param password
      * @return
      * @throws Exception
      */
     @Transactional(noRollbackForClassName = "ManageException")
-    public User login(HttpServletRequest request, String username, String password) throws ManageException {
-        Map<String, List> data = new HashMap<>();
+    public User iJklogin(HttpServletRequest request, String loginCode, String password) throws ManageException {
         //判断登陆信息是否正确
-        User user = userService.findByCode(username);
+        User user = userService.findByCode(loginCode);
         if (user == null) {
-            //保存登陆信息
-            String message = "账号不存在";
-            throw new ManageException(message);
+            //i健康登录认证
+            Map<String, Object> data = oauthIjkLogin(loginCode, password);
+            if (data!=null ) {
+                user = new User();
+                user.setPassword(password);
+                user.setLoginCode((String) data.get("user"));
+                user.setName((String) data.get("name"));
+                user.setGender((String) data.get("gender"));
+                user.setIdCardNo((String) data.get("idcard"));
+                user.setTelephone((String) data.get("mobile"));
+                userService.saveOrUpdate(user,LoginInfo.SAVE_TYPE_IJK);
+
+            }else {
+                String message = "账号不存在";
+                throw new ManageException(message);
+            }
         }
         if (!user.getPassword().equals(MD5.GetMD5Code(password + user.getSalt()))) {
             //保存登陆信息
@@ -47,11 +102,38 @@ public class LoginService {
         }
         request.getSession().setAttribute(LoginInfo.IS_LOGIN, true);
         request.getSession().setAttribute(LoginInfo.TOKEN, ""); //TODO token是否添加
-        request.getSession().setAttribute(LoginInfo.USER_NAME, user.getName());
+        request.getSession().setAttribute(LoginInfo.LOGIN_NAME, user.getName());
         request.getSession().setAttribute(LoginInfo.USER_ID, user.getId());
         user.setActivated(HouseUserContant.activated_active);
-        userService.saveOrUpdate(user,password,"systemLogin");
+        userService.saveOrUpdate(user,"systemLogin");
         return user;
+    }
+
+
+    /**
+     *  i健康用户信息认证
+     * @param username
+     * @param password
+     * @return
+     * @throws ManageException
+     */
+    public Map<String, Object> oauthIjkLogin(String username, String password) throws ManageException{
+        HashMap<String, Object> userDetail = null;
+        HttpHeaders reqHeaders = new HttpHeaders();
+        reqHeaders.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+        MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
+        params.add("client_id", clientId);
+        params.add("username", username);
+        params.add("password", password);
+        params.add("login_type", "1");
+        HttpEntity<MultiValueMap<String, String>> httpEntity = new HttpEntity<>(params, reqHeaders);
+        HashMap<String, Object> result = restTemplate.postForObject("http://svr-authentication:10260/oauth/login", httpEntity, HashMap.class);
+        if (200 == (Integer) result.get("status")){
+            userDetail =  (HashMap)result.get("obj");
+        return userDetail;
+        }else {
+            throw new ManageException("i健康用户认证失败");
+        }
     }
 
 
@@ -79,10 +161,10 @@ public class LoginService {
         }
         request.getSession().removeAttribute(LoginInfo.IS_LOGIN);
         request.getSession().removeAttribute(LoginInfo.TOKEN); //TODO token是否添加
-        request.getSession().removeAttribute(LoginInfo.USER_NAME);
+        request.getSession().removeAttribute(LoginInfo.LOGIN_NAME);
         request.getSession().removeAttribute(LoginInfo.USER_ID);
         user.setActivated(HouseUserContant.activated_offline);
-        userService.saveOrUpdate(user,password,"systemLogin");
+        userService.saveOrUpdate(user,"systemLogin");
         return user;
     }
 
