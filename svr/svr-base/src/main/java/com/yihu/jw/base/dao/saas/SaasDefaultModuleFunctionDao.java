9e847dbd81d63a066c713c0f38848f54e8bcb1fe
zdm <763558454@qq.com>
2018-10-11 17:48:55 +0800

    temp

--- svr/svr-base/src/main/java/com/yihu/jw/base/dao/saas/SaasDefaultModuleFunctionDao.java
+++ svr/svr-base/src/main/java/com/yihu/jw/base/dao/saas/SaasDefaultModuleFunctionDao.java
@@ -3,6 +3,8 @@ package com.yihu.jw.base.dao.saas;
 import com.yihu.jw.entity.base.saas.SaasDO;
 import com.yihu.jw.entity.base.saas.SaasDefaultModuleFunctionDO;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
 import java.util.List;
@@ -15,5 +17,7 @@ public interface SaasDefaultModuleFunctionDao extends PagingAndSortingRepository
 
     List<SaasDefaultModuleFunctionDO> findBySaasType(Integer saasType);
 
+    @Modifying
+    @Query("delete from SaasDefaultModuleFunctionDO sdf where sdf.saasType = ?1 ")
     void deleteBySaasType(Integer saasType);
 }
